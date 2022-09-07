5c1aa3755afa7a145732b1f05926b27055f29a67
Kahsel <leshaksattong96@gmail.com>
2022-09-07 10:45:57 +0100

    successfully uplaoded

---
+++ setGet.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.0;
+
+// create contract raisePower
+contract raisePower{
+    //declaring state variable
+    uint a;
+    uint b;
+    uint c;
+    string d;
+
+    //create a function to set data
+    function set(uint _a, uint _b, string _d) public{
+        d = _d = 'The answer is: ';
+        a = _a;
+        b  = _b;
+        c = _a ** _b;
+    }
+
+    //create a function that gets out data
+    function get() public view returns(string, uint){
+        return (d, c);
+    }
+}
